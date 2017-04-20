c0dc1329663d6f60ea9147243d90cc44ab7278bd
roboticLuddite <underwood.paul@gmail.com>
2017-04-20 10:14:06 -0500

    updating enocdewav

--- src/recorder.js
+++ src/recorder.js
@@ -153,36 +153,22 @@ export class Recorder {
             }
 
             function encodeWAV(samples) {
-                let buffer = new ArrayBuffer(44 + samples.length * 2);
-                let view = new DataView(buffer);
+                var buffer = new ArrayBuffer(44 + 2 * samples.length);
+                var view = new DataView(buffer);
 
-                /* RIFF identifier */
-                writeString(view, 0, 'RIFF');
-                /* RIFF chunk length */
-                view.setUint32(4, 36 + samples.length * 2, true);
-                /* RIFF type */
-                writeString(view, 8, 'WAVE');
-                /* format chunk identifier */
-                writeString(view, 12, 'fmt ');
-                /* format chunk length */
+                writeString(view, 0, "RIFF");
+                view.setUint32(4, 32 + 2 * samples.length, true);
+                writeString(view, 8, "WAVE");
+                writeString(view, 12, "fmt ");
                 view.setUint32(16, 16, true);
-                /* sample format (raw) */
                 view.setUint16(20, 1, true);
-                /* channel count */
-                view.setUint16(22, numChannels, true);
-                /* sample rate */
+                view.setUint16(22, 1, true);
                 view.setUint32(24, sampleRate, true);
-                /* byte rate (sample rate * block align) */
-                view.setUint32(28, sampleRate * 4, true);
-                /* block align (channel count * bytes per sample) */
-                view.setUint16(32, numChannels * 2, true);
-                /* bits per sample */
+                view.setUint32(28, 2 * sampleRate, true);
+                view.setUint16(32, 2, true);
                 view.setUint16(34, 16, true);
-                /* data chunk identifier */
-                writeString(view, 36, 'data');
-                /* data chunk length */
-                view.setUint32(40, samples.length * 2, true);
-
+                writeString(view, 36, "data");
+                view.setUint32(40, 2 * samples.length, true);
                 floatTo16BitPCM(view, 44, samples);
 
                 return view;
